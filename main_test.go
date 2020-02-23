ba82b4509dee153695305b4fad468d52273aa310
BT <bt@alifacademy.tj>
2020-02-23 15:54:08 +0500

    fix: tests

--- main_test.go
+++ main_test.go
@@ -67,7 +67,77 @@ func Test_ServerIndex(t *testing.T) {
 	}
 }
 
-func Test_ServerImages(t *testing.T) {
+func Test_ServerHTML(t *testing.T) {
+	addr := "0.0.0.0:9933"
+	go func() {
+		log.Printf("start server")
+		err:= start(addr)
+		if err != nil {
+			t.Fatalf("can't start server: %v", err)
+		}
+	}()
+	time.Sleep(3 * time.Second)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("can't dial: %v", err)
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			t.Fatalf("can't close dial: %v", err)
+		}
+	}()
+	write := bufio.NewWriter(conn)
+	write.WriteString("GET /html.html HTTP/1.1\r\n")
+	write.WriteString("Host: localhost\r\n")
+	write.WriteString("\r\n")
+	write.Flush()
+	bytes, err := ioutil.ReadAll(conn)
+	if err != nil {
+		t.Fatalf("can't read response from server: %v", err)
+	}
+	response := string(bytes)
+	if !strings.Contains(response, "200 OK") {
+		t.Fatalf("just it be 200 OK: %s", response)
+	}
+}
+
+func Test_ServerTEXT(t *testing.T) {
+	addr := "0.0.0.0:9977"
+	go func() {
+		log.Printf("start server")
+		err:= start(addr)
+		if err != nil {
+			t.Fatalf("can't start server: %v", err)
+		}
+	}()
+	time.Sleep(3 * time.Second)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("can't dial: %v", err)
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			t.Fatalf("can't close dial: %v", err)
+		}
+	}()
+	write := bufio.NewWriter(conn)
+	write.WriteString("GET /text.txt HTTP/1.1\r\n")
+	write.WriteString("Host: localhost\r\n")
+	write.WriteString("\r\n")
+	write.Flush()
+	bytes, err := ioutil.ReadAll(conn)
+	if err != nil {
+		t.Fatalf("can't read response from server: %v", err)
+	}
+	response := string(bytes)
+	if !strings.Contains(response, "200 OK") {
+		t.Fatalf("just it be 200 OK: %s", response)
+	}
+}
+
+func Test_ServerImageHTML(t *testing.T) {
 	addr := "0.0.0.0:8870"
 	go func() {
 		log.Printf("start server")
@@ -99,5 +169,110 @@ func Test_ServerImages(t *testing.T) {
 	response := string(bytes)
 	if !strings.Contains(response, "200 OK") {
 		t.Fatalf("just it be 200 OK: %s", response)
+	}
+}
+
+func Test_ServerImages(t *testing.T) {
+	addr := "0.0.0.0:8892"
+	go func() {
+		log.Printf("start server")
+		err:= start(addr)
+		if err != nil {
+			t.Fatalf("can't start server: %v", err)
+		}
+	}()
+	time.Sleep(3 * time.Second)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("can't dial: %v", err)
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			t.Fatalf("can't close dial: %v", err)
+		}
+	}()
+	write := bufio.NewWriter(conn)
+	write.WriteString("GET /img/1.jpg HTTP/1.1\r\n")
+	write.WriteString("Host: localhost\r\n")
+	write.WriteString("\r\n")
+	write.Flush()
+	bytes, err := ioutil.ReadAll(conn)
+	if err != nil {
+		t.Fatalf("can't read response from server: %v", err)
+	}
+	response := string(bytes)
+	if !strings.Contains(response, "200 OK") {
+		t.Fatalf("just it be 200 OK: %s", response)
+	}
+}
+
+func Test_ServerPDF(t *testing.T) {
+	addr := "0.0.0.0:8852"
+	go func() {
+		log.Printf("start server")
+		err:= start(addr)
+		if err != nil {
+			t.Fatalf("can't start server: %v", err)
+		}
+	}()
+	time.Sleep(3 * time.Second)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("can't dial: %v", err)
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			t.Fatalf("can't close dial: %v", err)
+		}
+	}()
+	write := bufio.NewWriter(conn)
+	write.WriteString("GET /task.pdf HTTP/1.1\r\n")
+	write.WriteString("Host: localhost\r\n")
+	write.WriteString("\r\n")
+	write.Flush()
+	bytes, err := ioutil.ReadAll(conn)
+	if err != nil {
+		t.Fatalf("can't read response from server: %v", err)
+	}
+	response := string(bytes)
+	if !strings.Contains(response, "200 OK") {
+		t.Fatalf("just it be 200 OK: %s", response)
+	}
+}
+
+func Test_Server404(t *testing.T) {
+	addr := "0.0.0.0:9966"
+	go func() {
+		log.Printf("start server")
+		err:= start(addr)
+		if err != nil {
+			t.Fatalf("can't start server: %v", err)
+		}
+	}()
+	time.Sleep(3 * time.Second)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("can't dial: %v", err)
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			t.Fatalf("can't close dial: %v", err)
+		}
+	}()
+	write := bufio.NewWriter(conn)
+	write.WriteString("GET /404.html HTTP/1.1\r\n")
+	write.WriteString("Host: localhost\r\n")
+	write.WriteString("\r\n")
+	write.Flush()
+	bytes, err := ioutil.ReadAll(conn)
+	if err != nil {
+		t.Fatalf("can't read response from server: %v", err)
+	}
+	response := string(bytes)
+	if !strings.Contains(response, "") {
+		t.Fatalf("response just be nil: %s", response)
 	}
 }
